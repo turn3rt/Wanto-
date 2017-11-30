2fd5eb8f4eebe9808027d8e912430394ae3ea7ff
Turner Thornberry <turnerthornberry@Turners-New-MacBook-Pro.local>
2017-11-30 10:51:38 -0500

    First project commit

---
+++ Wanto/Helper.swift
@@ -0,0 +1,20 @@
+//
+//  Helper.swift
+//  Wanto
+//
+//  Created by Turner Thornberry on 11/30/17.
+//  Copyright © 2017 Turner Thornberry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Helper {
+    
+    
+    
+    
+}
+
+
---
+++ Wanto/InactiveCell.swift
@@ -0,0 +1,24 @@
+//
+//  InactiveCell.swift
+//  Wanto
+//
+//  Created by Turner Thornberry on 11/30/17.
+//  Copyright © 2017 Turner Thornberry. All rights reserved.
+//
+
+import UIKit
+
+class InactiveCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
